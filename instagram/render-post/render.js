98bd48750f65d28590798684c923d926b8611e11
Param Siddharth <contact@paramsid.com>
2022-10-13 17:50:43 +0530

    Realize `node-canvas` won't work here; Need to use something else (maybe Puppeteer?)
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- instagram/render-post/render.js
+++ instagram/render-post/render.js
@@ -1,9 +1,9 @@
 const fs = require('fs-extra');
 const path = require('path');
 const axios = require('axios');
-const { registerFont, createCanvas } = require('canvas');
+const { registerFont, createCanvas, loadImage } = require('canvas');
 
-const { DIMENSION } = require('./constants');
+const { DIMENSION, FONT_RATIO } = require('./constants');
 
 const loadFonts = async () => {
 	if (!process.env.FONTS_LOADED) {
@@ -62,14 +62,87 @@ const loadFonts = async () => {
 
 loadFonts();
 
-const render = async (text, path, font = null) => {
+const getSVG = (text, width, height, fontName, fontSize, light) => {
+	// HTML entitites
+	const escapedText = text
+		.replace(/\&/g, '&amp;').replace(/</g, '&lt;');
+
+	const svg = `
+	<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+		<foreignObject x="0" y="0" width="${width}" height="${height}">
+			<style>
+				
+
+			div.a {
+				margin: 20px 80px;
+				height: ${height}px;
+				font-weight: normal;
+				${
+					fontName != null
+					? `font-family: ${JSON.stringify(fontName)};`
+					: ''
+				}
+				font-style: italic;
+				font-weight: bold;
+				font-size: ${fontSize}px;
+				color: ${light ? 'black' : 'white'};
+				display: flex;
+			}
+
+			div.b {
+				margin: auto;
+			}
+			</style>
+			<div xmlns="http://www.w3.org/1999/xhtml">
+				<div class='a'>
+					<div class='b'>${escapedText}</div>
+				</div>
+			</div>
+		</foreignObject>
+	</svg>
+	`
+	.replace(/\n/g, '');
+
+	return svg;
+};
+
+const render = async (text, postPath, light = false, colour = '#d00', font = null) => {
 	await loadFonts();
 
 	let defaultFont = process.env.DEFAULT_FONT || null;
 
 	const height = DIMENSION,
-	      width  = DIMENSION;
+	      width  = DIMENSION,
+		  fontSize = DIMENSION * FONT_RATIO;
 	const canvas = createCanvas(width, height);
+	
+	const svg = getSVG(text, width, height, defaultFont, fontSize, light);
+
+	const ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = colour;
+	ctx.fillRect(0, 0, width, height);
+
+	const grad = ctx.createRadialGradient(
+		width / 2, height / 2,
+		width / 4,
+		width / 2, height / 2,
+		width * 4
+	);
+
+	grad.addColorStop(0, colour);
+	grad.addColorStop(0.5, light ? 'white' : 'black');
+
+	ctx.fillStyle = grad;
+	ctx.fillRect(0, 0, width, height);
+
+	// const img = await loadImage(`data:image/svg+xml,${svg}`);
+	const img = await loadImage(path.resolve(__dirname, 'text.svg')); // :/ Oopsie...
+	// console.log(`data:image/svg+xml,${svg}`);
+	ctx.drawImage(img, 0, 0);
+
+	const buf = canvas.toBuffer();
+	fs.writeFileSync(path.join(postPath, 'post.png'), buf);
 };
 
 module.exports = {
